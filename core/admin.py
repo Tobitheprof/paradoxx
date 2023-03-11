5cc9a1cda5c9adceec06c9ea355dac32fa59ce28
Tobi TheRevolutionary <tobiekundayo07@gmail.com>
2023-03-11 21:31:56 +0100

    Last Commit before review

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import *
+from django_summernote.admin import SummernoteModelAdmin
+
+class SlideAdmin(admin.TabularInline):
+    model = Slide
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description', )
+
+class FlashcardAdmin(SummernoteModelAdmin, admin.ModelAdmin):
+    inlines = [SlideAdmin]
+    list_display = ['title', 'author', 'number_of_slides', 'date_created']
+    summernote_fields = ('description', )
 
 admin.site.register(Profile)
-admin.site.register(FlashCard)
-admin.site.register(Slide)
+admin.site.register(FlashCard, FlashcardAdmin)
+admin.site.register(Slide, PostAdmin)
 
 # Register your models here.
--- paradoxx/urls.py
+++ paradoxx/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]
 
 if settings.DEBUG:
